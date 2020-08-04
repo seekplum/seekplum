28322b55ce4da4c466a7a71c209545929dc62735
seekplum <1131909224@qq.com>
2020-08-04 20:07:23 +0800

    ADD: 解析git日志输出

---
+++ utils/parse_git_log.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+
+from __future__ import print_function
+import re
+
+"""
+通过 log 分析 git commit 命令
+"""
+# git log --name-only > test.txt 
+with open("test.txt", "r") as f:
+    content = f.read()
+pattern = re.compile(r'commit \w+\nAuthor: .*\nDate: .*\n\s+(.*)\s+\n([\S\n]+)\n', re.M)
+result = pattern.findall(content)
+for res in result[::-1]:
+    message = res[0]
+    files = " ".join(res[1].split("\n"))
+    print('git commit {}-m "{}"'.format(files, message))
+
--- utils/proxy_rewrite.py
+++ utils/proxy_rewrite.py
@@ -2,12 +2,17 @@
 # -*- coding: utf-8 -*-
 
 """
-1.安装Python包
+1.安装mitmproxy
 
 pip install mitmproxy==0.18.2
 
+或者
+
+brew install mitmproxy
+
 2.配置PAC脚本
 
+```js
 function FindProxyForURL(url, host) {
     // 百度转必应
     if (dnsDomainIs(host, "baidu.com") || dnsDomainIs(host, "www.baidu.com")) {
@@ -16,12 +21,21 @@ function FindProxyForURL(url, host) {
 
     return "DIRECT";
 }
+```
 
-3.启动服务,用 `mitmproxy`, `mitmdump`, `mitmweb` 这三个命令中的任意一个即可
+3.访问 http://mitm.it/ 下载证书
+
+下载后双击打开，`钥匙串` 选择 `系统`，之后配置 `始终信任`
+
+4.启动服务,用 `mitmproxy`, `mitmdump`, `mitmweb` 这三个命令中的任意一个即可
 
 mitmproxy -p 8123 -s proxy_rewrite.py --no-upstream-cert --insecure
 
-4.浏览器访问 http://www.baidu.com/ 显示必应官网
+不同版本注意命令行选项是否更改，如
+
+mitmdump -p 8123 -s proxy_rewrite.py --set upstream_cert=false --ssl-insecure
+
+5.浏览器访问 http://www.baidu.com/ 显示必应官网
 """
 
 import mitmproxy.script
@@ -36,8 +50,11 @@ TARGET_PORT = 80
 def request(flow):
     r = flow.request
     host = r.headers.get("Host", "")
-    print("Source host: {}, target host: {}".format(host, TARGET_HOST))
+    authority = r.headers.get(':authority', '')
+    print("Source host: {}, authority: {}, target host: {}".format(host, authority, TARGET_HOST))
     if host in (SOURCE_HOST,):
         # 必须先设置 host 再改写 header, 不然会被覆盖
         (r.scheme, r.host, r.port) = ("http", TARGET_HOST, TARGET_PORT)
         r.headers["Host"] = TARGET_HOST
+        # r.headers[":authority"] = authority
+
