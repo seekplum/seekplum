77aac07c2867b80b8565f660c2148c40af9f269b
seekplum <1131909224@qq.com>
2020-09-18 21:29:23 +0800

    MOD: openpyxl修改Excel

--- utils/pandas_csv.py
+++ utils/pandas_csv.py
@@ -2,7 +2,7 @@
 """
 # 安装依赖
 
-pip install pandas openpyxl
+pip install pandas openpyxl xlrd
 """
 import os
 import re
@@ -10,7 +10,13 @@ import glob
 import random
 import string
 
+from datetime import datetime
+
 import pandas as pd
+import openpyxl
+
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import PatternFill, Border
 
 
 def random_string(length=5):
@@ -100,6 +106,94 @@ def write_excel():
     df.to_excel(file_path, sheet_name="表格名字", index=False)
 
 
+def get_denominations(value):
+    if value is not None and str(value) != "nan" and str(value).startswith("减"):
+        return int(value[1:]) * 100
+    return None
+
+
+def check_create_time(creat_time):
+    return datetime(2020, 9, 16) < datetime.strptime(creat_time, "%Y-%m-%d %H:%M:%S") < datetime(2020, 9, 17, 23, 59,
+                                                                                                 59, 999)
+
+
+def pandas_update_excel(coupons=None):
+    coupons = coupons or []
+    excel_path = "/Users/seekplum/Downloads/优惠券列表.xlsx"
+    file_path = "/tmp/pandas-test.xlsx"
+    column_list = [3, 12, 19]
+
+    df = pd.read_excel(excel_path)
+    df_list = df.values.tolist()
+    for index, item in enumerate(df_list):
+        for col in column_list:
+            denominations = get_denominations(item[col])
+            if denominations is None:
+                continue
+            for coupon in coupons:
+                if not (coupon["denominations"] == denominations and check_create_time(coupon["creat_time"])):
+                    continue
+                old = df.iloc[index, col + 1]
+                old = f"{old}\n" if old and str(old) != "nan" else ""
+                df.iloc[index, col + 1] = f'{old}{coupon["activity_url"]}'
+    df.to_excel(file_path, sheet_name="表格名字", index=False)
+
+
+def write_cell(sheet, column, new_column, coupons):
+    col_values = list(sheet.columns)[column]
+    for row, cell in enumerate(col_values):
+        denominations = get_denominations(cell.value)
+        if denominations is None:
+            continue
+        new_row = row + 1
+        for coupon in coupons:
+            if not (coupon["denominations"] == denominations and check_create_time(coupon["creat_time"])):
+                continue
+
+            old = sheet.cell(new_row, new_column).value
+            old = f"{old}\n" if old and str(old) != "nan" else ""
+            new_value = f'{old}{coupon["activity_url"]}'
+
+            sheet.cell(new_row, new_column, new_value)
+
+
+def openpyxl_update_excel(coupons=None):
+    coupons = coupons or []
+    excel_path = "/Users/seekplum/Downloads/优惠券列表.xlsx"
+    file_path = "/tmp/openpyxl-test.xlsx"
+    column_list = [3, 12, 19]
+
+    book = openpyxl.load_workbook(excel_path)
+    sheet = book.active
+    title_row = 3
+    for index, column in enumerate(column_list):
+        new_column = column + 2
+        sheet.insert_cols(new_column)
+        # 设置列宽
+        sheet.column_dimensions[get_column_letter(new_column)].width = 120
+
+        # 设置标题
+        sheet.cell(title_row, new_column, "领取链接")
+
+        # 设置背景颜色
+        title_cell = sheet.cell(title_row, column)
+        title_fill = title_cell.fill
+        title_border = title_cell.border
+        cell = sheet.cell(title_row, new_column)
+        cell.fill = PatternFill("solid", fgColor=title_fill.fgColor, bgColor=title_fill.bgColor)
+        cell.border = Border(left=title_border.left, right=title_border.right, top=title_border.top,
+                             bottom=title_border.bottom)
+
+        write_cell(sheet, column, new_column, coupons)
+
+    write_cell(sheet, 2, 5, coupons)
+
+    book.save(file_path)
+
+
 if __name__ == '__main__':
     read_csv()
     write_excel()
+    coupons = []
+    pandas_update_excel(coupons)
+    openpyxl_update_excel(coupons)
