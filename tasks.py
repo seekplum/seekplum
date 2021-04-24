331df441db5e59963e861fe8e0901037dfcd8cdd
seekplum <1131909224@qq.com>
2021-04-24 12:00:34 +0800

    MOD: 更新依赖

--- tasks.py
+++ tasks.py
@@ -39,20 +39,6 @@ def covert_source(source):
     return source
 
 
-@task
-def clean(ctx):
-    """清除项目中无效文件"""
-    ctx.run("rm -rf build dist", echo=True)
-    ctx.run("find . -name '*.pyc' -exec rm -f {} +", echo=True)
-    ctx.run("find . -name '*.pyo' -exec rm -f {} +", echo=True)
-    ctx.run("find . -name '__pycache__' -exec rm -rf {} +", echo=True)
-    ctx.run("find . -name 'htmlcov' -exec rm -rf {} +", echo=True)
-    ctx.run("find . -name '.coverage' -exec rm -rf {} +", echo=True)
-    ctx.run("find . -name '.pytest_cache' -exec rm -rf {} +", echo=True)
-    ctx.run("find . -name '.benchmarks' -exec rm -rf {} +", echo=True)
-    ctx.run("find . -name '*.egg-info' -exec rm -rf {} +", echo=True)
-
-
 @task(clean)
 def sdist(ctx):
     ctx.run("python setup.py sdist", echo=True)
